35d531398a492cb514cf5780df9f6e0cc5c874d0
João Corrêa <126816260+JoaoCorreaSilva@users.noreply.github.com>
2023-06-01 11:10:28 -0300

    Add files via upload

--- Projeto Integrador João Corrêa/Site.js
+++ Projeto Integrador João Corrêa/Site.js
@@ -4,74 +4,62 @@ document.addEventListener('DOMContentLoaded', function() {
     $('.carousel').carousel();
 });
 
-let cart = [];
-let modalQt = 0;
-let key = 0;
+// Definir variáveis universais
+var valorTotal = [0, 0, 0, 0];
+var valorProduto = [0, 500, 5, 3000];
+var qtd = [0, 0, 0, 0];
 
-document.addEventListener('DOMContentLoaded', function() {
-    const buyButtons = document.querySelectorAll('.buy-button');
-    const cartItems = document.getElementById('cart-items');
-    const cartTotal = document.getElementById('cart-total');
-    const cartModal = document.getElementById('cart-modal');
-  
-    let cart = [];
-  
-    // Atualiza o carrinho
-    function updateCart() {
-      cartItems.innerHTML = '';
-      let total = 0;
-  
-      cart.forEach(item => {
-        const cartRow = document.createElement('tr');
-        const itemName = document.createElement('td');
-        const itemPrice = document.createElement('td');
-  
-        itemName.innerText = item.name;
-        itemPrice.innerText = `R$ ${item.price.toFixed(2)}`;
-  
-        cartRow.appendChild(itemName);
-        cartRow.appendChild(itemPrice);
-  
-        cartItems.appendChild(cartRow);
-  
-        total += item.price;
-      });
-  
-      cartTotal.innerText = `R$ ${total.toFixed(2)}`;
-    }
-  
-    // Adiciona um item ao carrinho
-    function addToCart(name, price) {
-      const item = {
-        name: name,
-        price: price
-      };
-  
-      cart.push(item);
-      updateCart();
-    }
-  
-    // Evento de clique nos botões de compra
-    buyButtons.forEach(button => {
-      button.addEventListener('click', function() {
-        const name = this.getAttribute('data-name');
-        const price = parseFloat(this.getAttribute('data-price'));
-        addToCart(name, price);
-        cartModal.style.display = 'block';
-      });
-    });
-  });
-  window.addEventListener('DOMContentLoaded', function() {
-    // Seletor para o botão de compra
-    var botaoCompra = document.querySelector('buy-button');
-  
-    // Adicionar o ouvinte de evento de clique
-    botaoCompra.addEventListener('click', function() {
-      // Obter a quantidade do elemento de entrada
-      var quantidadeElemento = document.querySelector('buy-button');
-      var quantidade = parseInt(quantidadeElemento.value);
-  
-      // Fazer algo com a quantidade
-      console.log('Quantidade:', quantidade);
-    });
-  });
+// Aumentar a quantidade de itens de um produto
+function adicionarItem(item) {
+	var quantidade = document.getElementById('quantidade' + item);
+	var total = document.getElementById('total' + item);
+	qtd[item] += 1;
+	valorTotal[item] = valorProduto[item] * qtd[item];
+	quantidade.innerHTML = qtd[item];
+	total.innerHTML = valorTotal[item];
+	console.log(quantidade);
+	valorCompra();
+}
+
+// Diminuir a quantidade de itens de um produto
+function removerItem(item) {
+	if (qtd[item] > 0) {
+		qtd[item] -= 1;
+		var quantidade = document.getElementById('quantidade' + item);
+		var total = document.getElementById('total' + item);
+		quantidade.innerHTML = qtd[item];
+		valorTotal[item] = valorProduto[item] * qtd[item];
+		total.innerHTML = valorTotal[item];
+		valorCompra();
+	}
+}
+
+// Remover o produto
+function removerProduto(produto) {
+	var pai = document.getElementById('carrinho');
+	var filho = document.getElementById('produto' + produto);
+	if (confirm('Confirmar exclusão?')) {
+		valorTotal[produto] -= (valorProduto[produto] * qtd[produto]);
+		qtd[produto] = 0;
+		pai.removeChild(filho);
+		valorCompra();
+	}
+}
+
+// Calcular o valor total da compra
+function valorCompra() {
+	var valorTotalCompra = document.getElementById('valorTotalCompra');
+	var valor = 0;;
+	for (var i = 0; i < valorTotal.length; i++) {
+		valor += valorTotal[i];
+	}
+	valorTotalCompra.innerHTML = valor;
+	console.log(valorTotal.length);
+}
+
+// Finalizar a compra e zerar os valores
+function finalizarCompra() {
+	var nome = document.getElementById('nome').innerHTML;
+	alert('Obrigada pela compra, ' + nome + '!');
+	document.location.reload(true);
+}
