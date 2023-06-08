f4bd10b85707113cec470d619facd2b060273002
João Corrêa <126816260+JoaoCorreaSilva@users.noreply.github.com>
2023-06-08 13:42:59 -0300

    Add files via upload

--- Projeto Integrador João Corrêa/Site.js
+++ Projeto Integrador João Corrêa/Site.js
@@ -1,65 +1,26 @@
-// Aguarde até que o documento HTML esteja totalmente carregado
+// Aguarde ate que o documento HTML esteja totalmente carregado
 document.addEventListener('DOMContentLoaded', function() {
     // Inicialize o carrossel
     $('.carousel').carousel();
 });
 
-// Definir variáveis universais
-var valorTotal = [0, 0, 0, 0];
-var valorProduto = [0, 500, 5, 3000];
-var qtd = [0, 0, 0, 0];
+// Definicao dos itens disponiveis
+var item1 = { name: "Body family", price: 58.90 };
+var item2 = { name: "Body Pecuaria", price: 19.90 };
+var item3 = { name: "Body FBI", price: 33.33 };
+var item4 = { name: "Body Modao", price: 19.90 };
+var item5 = { name: "Body baby timas", price: 1.99 };
+var item6 = { name: "Baby youtuber", price: 29.90 };
 
-// Aumentar a quantidade de itens de um produto
-function adicionarItem(item) {
-	var quantidade = document.getElementById('quantidade' + item);
-	var total = document.getElementById('total' + item);
-	qtd[item] += 1;
-	valorTotal[item] = valorProduto[item] * qtd[item];
-	quantidade.innerHTML = qtd[item];
-	total.innerHTML = valorTotal[item];
-	console.log(quantidade);
-	valorCompra();
-}
-
-// Diminuir a quantidade de itens de um produto
-function removerItem(item) {
-	if (qtd[item] > 0) {
-		qtd[item] -= 1;
-		var quantidade = document.getElementById('quantidade' + item);
-		var total = document.getElementById('total' + item);
-		quantidade.innerHTML = qtd[item];
-		valorTotal[item] = valorProduto[item] * qtd[item];
-		total.innerHTML = valorTotal[item];
-		valorCompra();
-	}
-}
-
-// Remover o produto
-function removerProduto(produto) {
-	var pai = document.getElementById('carrinho');
-	var filho = document.getElementById('produto' + produto);
-	if (confirm('Confirmar exclusão?')) {
-		valorTotal[produto] -= (valorProduto[produto] * qtd[produto]);
-		qtd[produto] = 0;
-		pai.removeChild(filho);
-		valorCompra();
-	}
-}
-
-// Calcular o valor total da compra
-function valorCompra() {
-	var valorTotalCompra = document.getElementById('valorTotalCompra');
-	var valor = 0;;
-	for (var i = 0; i < valorTotal.length; i++) {
-		valor += valorTotal[i];
-	}
-	valorTotalCompra.innerHTML = valor;
-	console.log(valorTotal.length);
-}
-
-// Finalizar a compra e zerar os valores
-function finalizarCompra() {
-	var nome = document.getElementById('nome').innerHTML;
-	alert('Obrigada pela compra, ' + nome + '!');
-	document.location.reload(true);
+// Funcao para adicionar item ao carrinho
+function addItemToCart(item) {
+    var cart = localStorage.getItem("cart");
+    if (cart) {
+        cart = JSON.parse(cart);
+    } else {
+        cart = [];
+    }
+    cart.push(item);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert("Item adicionado ao carrinho!");
 }
